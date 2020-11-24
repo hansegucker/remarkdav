e96ada7b8152e50719f673ad8146a4e22bf2d391
Jonathan Weth <git@jonathanweth.de>
2020-11-24 20:17:35 +0100

    Let CI lint

--- remarkdav/cli.py
+++ remarkdav/cli.py
@@ -1,14 +1,16 @@
 import click
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.option("--count", default=1, help="Number of greetings.")
 @click.option("--name", prompt="Your name", help="The person to greet.")
 def sync(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
+    """Greet NAME for a total of COUNT times."""
     for x in range(count):
         click.echo("Hello %s!" % name)
 
